19edaa855c96f39e589a70341922f3648e37bb28
John S <jan.snopek.86@gmail.com>
2017-05-23 22:07:57 +0200

    SORTOWANIE BABELKOWE

--- TCP_CLIENT_V2/TCP_CLIENT_V2.cpp
+++ TCP_CLIENT_V2/TCP_CLIENT_V2.cpp
@@ -116,3 +116,24 @@ int main()
 	WSACleanup();
 	return 0;
 }// by Johnny
+
+
+// Sortowanie Babelkowe
+/*
+void sortowanie_babelkowe(int *tab, int n)
+{
+	for (int i = 1; i<n; i++)
+	{
+		for (int j = n - 1; j >= 1; j--)
+		{
+			if (tab[j]<tab[j - 1])
+			{
+				int bufor;
+				bufor = tab[j - 1];
+				tab[j - 1] = tab[j];
+				tab[j] = bufor;
+			}
+		}
+	}
+}
+*/
